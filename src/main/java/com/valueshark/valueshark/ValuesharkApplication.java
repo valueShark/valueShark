f81976db239400cddaae33baf2a9fc6886b01f76
Chase Tucker <tuck8106@gmail.com>
2020-01-30 14:39:37 -0800

    repo setup

---
+++ src/main/java/com/valueshark/valueshark/ValuesharkApplication.java
@@ -0,0 +1,13 @@
+package com.valueshark.valueshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuesharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuesharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valueshark/valueshark/ValuesharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valueshark.valueshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuesharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
