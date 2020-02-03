244b0b916a8ee90ee359b01ef49547f6b4b0dfb2
Lucas Wilber <lucasjwilber@gmail.com>
2020-02-03 14:40:22 -0800

    added portfolio item

---
+++ src/main/java/com/valueshark/valueshark/PortfolioItem.java
@@ -0,0 +1,28 @@
+package com.valueshark.valueshark;
+
+import javax.persistence.*;
+
+@Entity
+public class PortfolioItem {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public ApplicationUser userId;
+
+//    @ManyToOne
+//    @JoinColumn(name = "company_id")
+//    public Company companyId;
+
+    int shares;
+    double pricePerShare;
+
+    PortfolioItem() {};
+    PortfolioItem(long userId, long companyId, int shares, double pricePerShare) {
+
+    }
+
+}
