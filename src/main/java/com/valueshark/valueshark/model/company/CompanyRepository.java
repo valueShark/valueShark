9ac06a05ce3cf0eb85f09ad6aae1b0a7a2df8a13
Chase Tucker <tuck8106@gmail.com>
2020-02-04 11:23:46 -0800

    created route for searchbar

--- src/main/java/com/valueshark/valueshark/model/company/CompanyRepository.java
+++ src/main/java/com/valueshark/valueshark/model/company/CompanyRepository.java
@@ -3,4 +3,5 @@ package com.valueshark.valueshark.model.company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+  Company getBySymbol(String symbol);
 }
