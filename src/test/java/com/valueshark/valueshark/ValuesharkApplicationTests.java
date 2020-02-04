59f942a44efd210482c3f85570736cad9afea312
RAN VAKNIN <ranvaknin0@gmail.com>
2020-02-04 11:38:19 -0800

    integration test2

--- src/test/java/com/valueshark/valueshark/ValuesharkApplicationTests.java
+++ src/test/java/com/valueshark/valueshark/ValuesharkApplicationTests.java
@@ -1,14 +1,19 @@
 package com.valueshark.valueshark;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ValuesharkApplicationTests {
 
+	@Autowired
+	ValueSharkController controller;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(controller).isNotNull();
 	}
-	
 
 }
