f9c6e50ccfba8090f01198363a6b9e519c8f58ab
Chase Tucker <tuck8106@gmail.com>
2020-02-04 09:50:00 -0800

    resolved conflicts

--- src/main/java/com/valueshark/valueshark/model/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.valueshark.valueshark.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-}
